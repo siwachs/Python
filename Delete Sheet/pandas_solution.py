681b480f20f727e4ed2a9d7615bd06a8b6ba89bd
siwachs <ssiwach.8888@gmail.com>
2024-01-13 13:01:13 +0530

    delete sheet in a workbook

--- Delete Sheet/pandas_solution.py
+++ Delete Sheet/pandas_solution.py
@@ -1,18 +1,34 @@
-from openpyxl import load_workbook
+import openpyxl
+import xlwings as xw
 
 
-def delete_sheet_by_name(file_path, sheet_name):
-    wb = load_workbook(file_path)
+def fix_and_load_excel(file_path, sheet_name):
+    try:
+        app = xw.App(visible=False)  # Open Excel in the background
+        workbook = app.books.open(file_path)
+        workbook.save()
+        workbook.close()
+        app.quit()
+        repaired_workbook = openpyxl.load_workbook(file_path)
+        print("Workbook repaired and loaded successfully.")
 
-    if sheet_name in wb.sheetnames:
-        sheet = wb[sheet_name]
-        wb.remove(sheet)
-        wb.save(file_path)
+        sheet = repaired_workbook[sheet_name]
+
+        # It clear sheet sheet name remain but content completly removed
+        for row in sheet.iter_rows(min_row=1, max_row=sheet.max_row, min_col=1, max_col=sheet.max_column):
+            for cell in row:
+                cell.value = None
+        
+        # It corroupt drawing.xml and result in file and data loss
+        if sheet_name in repaired_workbook.sheetnames:
+            del repaired_workbook[sheet_name]
+        repaired_workbook.save(file_path)
         print(f"The sheet '{sheet_name}' has been deleted.")
-    else:
-        print(f"The sheet '{sheet_name}' does not exist in the workbook.")
+
+    except Exception as e:
+        print(f"Error: {e}")
 
 
-file_path = r'E:\Projects\Python\Delete Sheet\1.xlsx'
-sheet_name_to_delete = 'Evaluation Warning'
-delete_sheet_by_name(file_path, sheet_name_to_delete)
+if __name__ == "__main__":
+    file_path = r'E:\Projects\Python\Delete Sheet\1.xlsx'
+    fix_and_load_excel(file_path, "Evaluation Warning")
